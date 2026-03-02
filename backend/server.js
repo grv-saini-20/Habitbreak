7c3149eb1669050b1326517fc094e0bc9eedd912
gourav <gourav@auvo.io>
2026-03-02 14:48:09 +0530

    feat: user schema created and password hashed is done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import  express from "express";
 import dotenv from "dotenv";
-import connectDb from "./config/db";
+import connectDb from "./config/db.js";
 
 dotenv.config();
 connectDb();
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
