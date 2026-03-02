fbcb60845f5f2e5b4708e86fefb5a674a2fb7fdf
gourav <gourav@auvo.io>
2026-03-02 14:15:51 +0530

    feat: Database connection added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import  express from "express";
 import dotenv from "dotenv";
+import connectDb from "./config/db";
 
 dotenv.config();
+connectDb();
 
 const app = express();
+
 
 const PORT = process.env.PORT || 5000;
 
