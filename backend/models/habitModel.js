40f9267f3e3cf8fbe86e9e396f89bf75e71d1318
gourav <gourav@auvo.io>
2026-03-12 11:51:57 +0530

    feat: habit schema added

---
+++ backend/models/habitModel.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const habitSchema = new mongoose.Schema (
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User"
+        },
+
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        description: {
+            type: String,
+            required: true
+        },
+
+        type : {
+            type: String,
+            enum: ["good", "bad"],
+            required: true
+        },
+
+        frequency: {
+            type: String,
+            enum: ["daily", "weekly"],
+            default: "daily"
+        },
+
+        currentStreak: {
+            type: Number,
+            default: 0,
+        },
+
+        longestStreak: {
+            type: Number,
+            default: 0,
+        },
+
+        isArchived: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    {timestamps: true}
+)
+
+const Habit = mongoose.model("Habit", habitSchema);
+export default Habit;
