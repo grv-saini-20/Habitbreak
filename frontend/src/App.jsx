046921fa78da3ad4af359ab1115c8a3897e48afa
gourav <gourav@auvo.io>
2026-03-13 10:24:42 +0530

    feat: sidebar component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import { Outlet } from 'react-router-dom'
-import './App.css'
+import { Outlet } from "react-router-dom"
+import "./App.css"
+
+import { SidebarProvider } from "@/components/ui/sidebar"
+import { AppSidebar } from "./components/project/AppSidebar"
 
 function App() {
   return (
-    <>
-      <Outlet/>
-    </>
+    <SidebarProvider>
+      <main className="w-full flex">
+        <AppSidebar />
+        <div className="flex-1">
+          <Outlet />
+        </div>
+      </main>
+    </SidebarProvider>
   )
 }
 
---
+++ frontend/src/components/project/AppSidebar.jsx
@@ -0,0 +1,56 @@
+import { Home, ListTodo } from "lucide-react"
+import { NavLink } from "react-router-dom"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// import { ModeToggle } from "@/components/mode-toggle"
+
+export function AppSidebar() {
+  return (
+    <Sidebar className={"p-6"}>
+
+      <SidebarHeader>
+        <h2 className="text-lg font-semibold">HabitBreak</h2>
+      </SidebarHeader>
+
+      <SidebarContent>
+
+        <SidebarMenu>
+
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <NavLink to="/">
+                <Home />
+                Dashboard
+              </NavLink>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <NavLink to="/habits">
+                <ListTodo />
+                Habits
+              </NavLink>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+
+        </SidebarMenu>
+
+      </SidebarContent>
+
+      <SidebarFooter>
+        {/* <ModeToggle /> */}
+      </SidebarFooter>
+
+    </Sidebar>
+  )
+}
